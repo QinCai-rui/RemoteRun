dd363365ef6b06f0d564693527ed895aa9846753
QinCai-rui <140027854+QinCai-rui@users.noreply.github.com>
2025-08-04 17:37:52 +1200

    use env var and fix stuff

--- examples/add_server.sh
+++ examples/add_server.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-# Add a new server (replace _TOKEN_ with your JWT, and other values as needed)
 curl -X POST http://localhost:8012/servers \
-  -H "Authorization: Bearer _TOKEN_" \
+  -H "Authorization: Bearer ${TOKEN}" \
   -H "Content-Type: application/json" \
-  -d '{
-    "name": "this-is-my-server-yaya",
-    "host": "192.168.1.100",
-    "ssh_username": "user",
-    "ssh_password": "linux on top"
-  }'
+  -d @- <<EOF
+{
+  "name": "${SERVER_NAME}",
+  "host": "${SERVER_HOST}",
+  "ssh_username": "${SERVER_USER}",
+  "ssh_privkey": "${SERVER_PRIVKEY}"
+}
+EOF
