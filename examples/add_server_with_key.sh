61353569121a26302716a96ecf70ee82acb2736c
QinCai-rui <140027854+QinCai-rui@users.noreply.github.com>
2025-08-04 17:56:32 +1200

    fix ssh key stuff

--- examples/add_server_with_key.sh
+++ examples/add_server_with_key.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-# this is bad. use add_server_with_key.sh instead
+# Add a server using the test SSH private key
+# need these environment variables before running:
+# TOKEN, SERVER_NAME, SERVER_HOST, SERVER_USER
+
+# Load the private key from file. this is mine
+SERVER_PRIVKEY="$(cat /home/qincai/RemoteRun/examples/test_ed25519)"
+
 curl -X POST http://localhost:8012/servers \
   -H "Authorization: Bearer ${TOKEN}" \
   -H "Content-Type: application/json" \
