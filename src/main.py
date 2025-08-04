638bafe69c141bdea64e862a2ef5a4250b0e050f
QinCai-rui <140027854+QinCai-rui@users.noreply.github.com>
2025-08-04 15:58:07 +1200

    Change port to 8012 cos 8000 is way to common

--- src/main.py
+++ src/main.py
@@ -418,4 +418,4 @@ def get_allowlist(current_user: UserDB = Depends(get_current_user)):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8012, reload=True)
